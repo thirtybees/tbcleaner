9e5b084bb590fd01e5fb74bdcc03d70f59d553d5
Petr Hučík <petr@getdatakick.com>
2022-12-13 14:54:16 +0100

    Remove unused dynamic property

--- tbcleaner.php
+++ tbcleaner.php
@@ -847,7 +847,6 @@ class TbCleaner extends Module
         $lang = new Language((int) Configuration::get('PS_LANG_DEFAULT'));
         $helper->default_form_language = $lang->id;
         $helper->allow_employee_form_lang = Configuration::get('PS_BO_ALLOW_EMPLOYEE_FORM_LANG') ? Configuration::get('PS_BO_ALLOW_EMPLOYEE_FORM_LANG') : 0;
-        $this->fields_form = [];
         $helper->id = (int) Tools::getValue('id_carrier');
         $helper->identifier = $this->identifier;
         $helper->submit_action = 'btnSubmit';
